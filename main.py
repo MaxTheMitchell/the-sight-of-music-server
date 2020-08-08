cc3be29b29c7e73c47c737f93a4d2cb647c1f159
MaxTheMitchell <rat2043@gmail.com>
2020-08-07 21:06:58 -0700

    add custom img uploading

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ REDIRECT_URI = os.getenv("REDIRECT_URI")+"/authorize/code"
 
 auth = AuthorizationCode(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,'user-read-currently-playing')
 current_song = CurrentlyPlaying(auth)
+img_url = ""
 
 @app.route('/')
 def main():
     if auth.is_fully_initalized():
-        return (open("front/main.html","rb").read()+ \
-             bytes("<h1>Currently Playing:\n</h1><img src={}>".format(
-                 current_song.get_cover640()),'utf-8'))
+        return (
+            bytes("<script>let ownImg = {}</script>".format(str(img_url != "").lower()),'utf-8')+
+            open("front/main.html","rb").read()+ \
+             bytes("<h1>Currently Playing:\n</h1><img class='main_img' src={}>".format(
+                #  current_song.get_cover640()
+                get_img(False)
+                 ),'utf-8'))
     return flask.redirect('/authorize')
 
 @app.route('/authorize')
@@ -35,15 +40,15 @@ def make_tokens():
 
 @app.route('/image/pixles')
 def get_pixles_in_album():
-    return str(ImageAnalyser(current_song.get_cover64()).get_pixles())
+    return str(ImageAnalyser( get_img()).get_pixles())
 
 @app.route('/image/pixles/<numb>')
 def get_numb_pixles(numb):
-    return str(ImageAnalyser(current_song.get_cover64()).get_pixles(int(numb)))
+    return str(ImageAnalyser( get_img()).get_pixles(int(numb)))
 
 @app.route('/image/pixles/<numb>/section')
 def get_section(numb):
-    return (ImageAnalyser(current_song.get_cover64()).get_subsection_of_pixles(
+    return str(ImageAnalyser( get_img()).get_subsection_of_pixles(
         int(numb),
         int(flask.request.args.get('start', None)),
         int(flask.request.args.get('end', None)),
@@ -52,7 +57,7 @@ def get_section(numb):
 
 @app.route('/image/pixles/<numb>/section/reversed')
 def get_reversed_section(numb):
-    return str(ImageAnalyser(current_song.get_cover64()).get_half_reversed_subsection_of_pixles(
+    return str(ImageAnalyser( get_img()).get_half_reversed_subsection_of_pixles(
         int(numb),
         int(flask.request.args.get('start', None)),
         int(flask.request.args.get('end', None))
@@ -62,12 +67,25 @@ def get_reversed_section(numb):
 def get_image_at_resolution(resolution):
     resolution = int(resolution)
     if resolution < 64:
-        img = current_song.get_cover64()
+        img =  get_img()
     elif resolution < 300:
-        img = current_song.get_cover300()
+        img =  get_img(False)
     else:
         return "{} is an invalid resolution".format(resolution)
     return ImageAnalyser(img).get_html_askii_display(resolution)
 
+@app.route('/image/set')
+def set_img_url():
+    global img_url
+    img_url = flask.request.args.get('img_url', default = "")
+    return bytes(img_url,'utf-8')
+
+def get_img(sixty_four=True):
+    global img_url
+    if img_url != "":
+        return img_url
+    elif sixty_four:
+        return current_song.get_cover64()
+    return current_song.get_cover640()
 
 app.run(host="0.0.0.0",port=PORT)
